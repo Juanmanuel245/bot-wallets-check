1fcffc75f8ba2b1faabe07f9ac88913945e5a257
Juanma <juanmanuelcaffi@gmail.com>
2023-09-02 21:04:47 -0300

    Se modificaron los campos que estaban expuestos

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bot-wallets-check
-Bot de Telegram que chequea en determinadas wallets si tienen una determinada cantidad de tokens.
+Bot de Telegram que chequea un contrato determinado de la red de Polygon si una lista determinada de wallets tienen menos de 10 tokens.
---
+++ walletAddresses.js
@@ -0,0 +1,19 @@
+// walletAddresses.js
+const walletAddress = [
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+    {'wallet' : 'xxxxxxxxxxxxxxxxxxxxxxx', 'nombre' : 'xxx'},
+];
+
+module.exports = walletAddress;
